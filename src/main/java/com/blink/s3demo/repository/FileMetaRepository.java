9b2e962bfb7efe228cf40623be4e44a0115d7ab2
t3rry327 <65151897+t3rry327@users.noreply.github.com>
2023-01-19 12:23:07 -0300

    Delete src/main/java/com/blink/s3demo directory

--- src/main/java/com/blink/s3demo/repository/FileMetaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blink.s3demo.repository;
-
-import com.blink.s3demo.model.FileMeta;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileMetaRepository extends CrudRepository<FileMeta, Integer> {
-}
