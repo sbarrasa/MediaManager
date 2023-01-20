063df64e08425d135a5f2a9b506d52ce877dc847
t3rry327 <t3rry327@gmail.com>
2023-01-20 16:21:49 -0300

    Fix get in MediaController

--- src/main/java/com/blink/mediaserver/controller/MediaController.java
+++ src/main/java/com/blink/mediaserver/controller/MediaController.java
@@ -1,15 +1,14 @@
 package com.blink.mediaserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.blink.mediamanager.MediaTemplate;
-import com.blink.mediamanager.s3.S3Service;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,57 +17,56 @@ import java.util.List;
 @Controller
 public class MediaController {
 
-	@Autowired
-	private MediaTemplate mediaTemplate;
+    @Autowired
+    private MediaTemplate mediaTemplate;
 
-	@ResponseBody
-	@RequestMapping(path = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String upload(@RequestPart() MultipartFile multipartFile) throws IOException {
-		
-		return mediaTemplate.upload(toFile(multipartFile)); 
-	}
+    @ResponseBody
+    @RequestMapping(path = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String upload(@RequestPart() MultipartFile multipartFile) throws IOException {
 
-	private File toFile(MultipartFile multipartFile) {
-		File file = new File(multipartFile.getOriginalFilename());
-		try {
-			file.createNewFile();
-			FileOutputStream fos = new FileOutputStream(file);
-			try {
-				fos.write(multipartFile.getBytes());
-			} finally {
-				fos.close();
-			}
-		} catch (IOException e) {
-			file = null;
-		}
+        return mediaTemplate.upload(toFile(multipartFile));
+    }
 
-		return file;
-	}
+    private File toFile(MultipartFile multipartFile) {
+        File file = new File(multipartFile.getOriginalFilename());
+        try {
+            file.createNewFile();
+            FileOutputStream fos = new FileOutputStream(file);
+            try {
+                fos.write(multipartFile.getBytes());
+            } finally {
+                fos.close();
+            }
+        } catch (IOException e) {
+            file = null;
+        }
+
+        return file;
+    }
 
 
-	@GetMapping("delete/{id}")
-	@ResponseBody
-	public void delete(@PathVariable String id) {
-		mediaTemplate.delete(id);
-	}
+    @GetMapping("delete/{id}")
+    @ResponseBody
+    public void delete(@PathVariable String id) {
+        mediaTemplate.delete(id);
+    }
 
-	@GetMapping(value = "get/{filename}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	@ResponseBody
-	public FileSystemResource get(@PathVariable String id) {
+    @GetMapping(value = "get/{filename}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public String get(@PathVariable String filename) {
+        return mediaTemplate.getFullPath(filename);
+    }
 
-		return new FileSystemResource(mediaTemplate.getFullPath(id));
-	}
+    @GetMapping("listall_metadata")
+    @ResponseBody
+    public List<?> listallMetadata() {
+        return mediaTemplate.listAllMetadata();
+    }
 
-	@GetMapping("listall_metadata")
-	@ResponseBody
-	public List<?> listallMetadata() {
-		return mediaTemplate.listAllMetadata();
-	}
-
-	@GetMapping("listall")
-	@ResponseBody
-	public List<String> listall() {
-		return mediaTemplate.listAllFullPath();
-	}
+    @GetMapping("listall")
+    @ResponseBody
+    public List<String> listall() {
+        return mediaTemplate.listAllFullPath();
+    }
 
 }
