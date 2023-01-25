1d5cba4d3b79c69921c33a02a7319316a134ffd0
Sebastian Barrasa <sebastian.barrasa@blink-srl.com>
2023-01-25 14:20:10 -0300

    upload Async prototipo

--- src/main/java/com/blink/mediamanager/MediaTemplate.java
+++ src/main/java/com/blink/mediamanager/MediaTemplate.java
@@ -3,84 +3,86 @@ package com.blink.mediamanager;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.zip.CRC32;
 
+public interface MediaTemplate {
 
-public interface MediaTemplate  {
-	    
+	default public void upload(Media media, BiConsumer<Media, MediaStatus> callback) {
+		CompletableFuture.ejecutar(upload(media))
+			.cuandoTermineOk(callback.accept(media, MediaStatus.ok))
+			,cuandoTermineConError(callback.accept(media, MediaStatus.err(exception)));
+		
+	}
+	
 	default public String upload(Media media) {
 
-        if (!fileExistsInRemote(media)) {
+		if (!fileExistsInRemote(media)) {
 
-        	Boolean uploaded = uploadImpl(media);
-        	if(!uploaded)
-                throw new MediaError(String.format("Can't upload %s", media.getId()));
-        		
-        }
-        return getURL(media.getId());
+			Boolean uploaded = uploadImpl(media);
+			if (!uploaded)
+				throw new MediaError(String.format("Can't upload %s", media.getId()));
 
-    }
-    
-    
+		}
+		return getURL(media.getId());
+
+	}
+
+	default public List<String> upload(List<Media> medias) {
+		List<String> res = new ArrayList<>();
+		medias.forEach(media -> {
+			res.add(upload(media));
+		});
+		return res;
+	}
+
 	public void delete(String id) throws MediaException;
-    
-    default public void delete(List<String> ids) {
-        ids.forEach(id -> {
+
+	default public void delete(List<String> ids) {
+		ids.forEach(id -> {
 			try {
 				delete(id);
 			} catch (MediaException ignored) {
 			}
 		});
-    }
-    
-    default public List<String> listAllFullPath(){
-    	return listAllIDs()
-                   .stream().map(this::getURL).collect(Collectors.toList());
-      
-    }
-    
-    public List<String> listAllIDs();
-    
-    public List<?> listAllMetadata();
-      
-    public String getURL(String id);
-    
-    public default String getChecksum(Media media) {
-        CRC32 crc32 = new CRC32();
-        try {
-            crc32.update(media.getStream().readAllBytes());
-        } catch (IOException e) {
-            throw new MediaError(e);
-        }
-        return String.valueOf(crc32.getValue());
-    }
+	}
 
-    public String getRemoteChecksum(String id) ;	
+	default public List<String> listAllFullPath() {
+		return listAllIDs().stream().map(this::getURL).collect(Collectors.toList());
 
-    default public List<String> upload(List<Media> medias) {
-        List<String> res = new ArrayList<>();
-        medias.forEach(media -> {
-            res.add(upload(media));
-        });
-        return res;
-    }
+	}
 
-    
+	public List<String> listAllIDs();
 
-    default public boolean fileExistsInRemote(Media media) {
-        
-    	String remoteChecksum = getRemoteChecksum(media.getId());
-        
-    	String fileChecksum = getChecksum(media);
-          
-    	return fileChecksum.equals(remoteChecksum);
-    };
+	public List<?> listAllMetadata();
 
+	public String getURL(String id);
 
-    public Boolean uploadImpl(Media media);
-    
-    public Media get(String id) throws MediaException;
-    
-        
+	public default String getChecksum(Media media) {
+		CRC32 crc32 = new CRC32();
+		try {
+			crc32.update(media.getStream().readAllBytes());
+		} catch (IOException e) {
+			throw new MediaError(e);
+		}
+		return String.valueOf(crc32.getValue());
+	}
+
+	public String getRemoteChecksum(String id);
+
+	default public boolean fileExistsInRemote(Media media) {
+
+		String remoteChecksum = getRemoteChecksum(media.getId());
+
+		String fileChecksum = getChecksum(media);
+
+		return fileChecksum.equals(remoteChecksum);
+	};
+
+	public Boolean uploadImpl(Media media);
+
+	public Media get(String id) throws MediaException;
+
 }
